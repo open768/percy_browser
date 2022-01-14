3dd1046679799990dcd89ae720ad1d56c6e03d58
open seven six ate <open768@yahoo.co.uk>
2022-01-14 20:51:43 +0000

    is_localhost moved

--- php/common.php
+++ php/common.php
@@ -21,7 +21,7 @@
 
 	
 	//requests without www get redirected to http
-	if (!cHeader::is_localhost()){
+	if (!cDebug::is_localhost()){
 		if ( strpos($_SERVER["HTTP_HOST"],'www') === false){
 			$newURL = "http://www.".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
 			cHeader::redirect($newURL);
